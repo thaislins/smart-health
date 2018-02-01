6062536cc5154bb4c634892744b6b20beac9565a
Thais Lins <thaisfli@hotmail.com>
2018-02-01 04:21:44 -0300

    Add chart movement

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Chart } from 'chart.js';
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
     selector: 'page-home',
@@ -8,25 +9,26 @@ import { Chart } from 'chart.js';
 })
 export class HomePage {
 
-    @ViewChild('barCanvas1') barCanvas1;
-    @ViewChild('barCanvas2') barCanvas2;
+    @ViewChild('hrCanvas') hrCanvas;
+    @ViewChild('rrCanvas') rrCanvas; 
 
-    barChart1: any;
-    barChart2: any;
+    hrChart: any;
+    rrChart: any;
+    maxResults = 5;
+    count = 0;
 
     constructor(public navCtrl: NavController) {
-
+        setInterval(() => {this.addData(this.rrChart, this.count++, Math.floor(Math.random()*20 + 5))},1000);
     }
 
     ionViewDidLoad() {
 
-        this.barChart1 = new Chart(this.barCanvas1.nativeElement, {
+        this.hrChart = new Chart(this.hrCanvas.nativeElement, {
 
             type: 'line',
             data: {
                 labels: [1, 2, 3, 4, 5, 6, 7],
                 datasets: [{
-                    label: 'Heart Rate ',
                     data: [12, 19, 3, 5, 2, 3],
 
                     backgroundColor: 'rgba(255, 255, 255, 0.2)',
@@ -35,7 +37,7 @@ export class HomePage {
                 }]
             },
 
-/*            options: {
+            options: {
                 scales: {
                     yAxes: [{
                         ticks: {
@@ -43,18 +45,18 @@ export class HomePage {
                         }
                     }]
                 }
-            }*/
+            }
 
         });
 
 
-        this.barChart2 = new Chart(this.barCanvas2.nativeElement, {
+        this.rrChart = new Chart(this.rrCanvas.nativeElement, {
 
             type: 'line',
             data: {
                 labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                 datasets: [{
-                    data: [12, 19, 3, 5, 2, 3, 40, 111],
+                    data: [12, 19, 3, 5, 2, 3, 40, 111, 309, 100],
 
                     backgroundColor: 'rgba(255, 29, 111, 0.4)',
                     borderColor: 'rgba(255,50,132,1)',
@@ -62,7 +64,7 @@ export class HomePage {
                 }]
             },
 
-/*            options: {
+            options: {
                 scales: {
                     yAxes: [{
                         ticks: {
@@ -70,9 +72,20 @@ export class HomePage {
                         }
                     }]
                 }
-            }*/
+            }
 
         });
 
+    }
+
+
+    addData(chart, label, data) {
+        chart.data.labels.push(label);
+        chart.data.datasets.forEach(dataset => dataset.data.push(data));
+        if(chart.data.labels.length > this.maxResults) {
+            chart.data.labels.splice(0, 1);
+            chart.data.datasets.forEach(dataset => dataset.data.splice(0,1));
+        }
+        chart.update();
     }
 }
